b088ecf68ed0349d3c9e967f7df9d3642d72287f
Mahmoud Fouad <mahmoudfouad443@gmail.com>
2021-05-15 14:39:24 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mahmoud Fouad on 5/15/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
